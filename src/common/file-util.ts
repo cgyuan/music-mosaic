19e200ec27ce5c37c6209d45f42b076e286c1d9a
cgyuan <2515380940@qq.com>
2024-12-01 19:34:36 +0800

    refactor(file-util): normalize file paths across OS

--- src/common/file-util.ts
+++ src/common/file-util.ts
@@ -1,19 +1,22 @@
 import { is } from "./is";
 
 export function addFileScheme(filePath: string) {
+  const normalizedPath = filePath.replace(/\\/g, '/');
+  
   if (!is.windows()) {
-    return filePath.startsWith("theme:")
-      ? filePath
-      : `theme://localhost/${filePath}`;
+    return normalizedPath.startsWith("theme:")
+      ? normalizedPath
+      : `theme://localhost/${normalizedPath}`;
   } else {
-    return filePath.startsWith("http")
-      ? filePath
-      : `http://theme.localhost/${filePath}`;
+    return normalizedPath.startsWith("http")
+      ? normalizedPath
+      : `http://theme.localhost/${normalizedPath}`;
   }
 }
 
 export function addTailSlash(filePath: string) {
-  return filePath.endsWith("/") || filePath.endsWith("\\")
-    ? filePath
-    : filePath + "/";
+  const normalizedPath = filePath.replace(/\\/g, '/');
+  return normalizedPath.endsWith("/")
+    ? normalizedPath
+    : normalizedPath + "/";
 }
